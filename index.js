1c5feb4e5381c72a7af499b59611a63fdf3b74f8
kosatun@aol.com <kosatun@aol.com>
2018-07-03 03:04:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function currentLine(katzDeliLine) {
   var line =  [];
   for (var i = 0; i < katzDeliLine.length; i++) {
     line.push(`${i + 1}. ${katzDeliLine[i]}`);
+  }
      return `The line is currently: ${line.join(", ")}`;
-    }
 }   
   
